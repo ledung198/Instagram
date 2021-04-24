818ef4775259e6b958b79ce3232a827dadaabe22
KZDeft <ingeni0usvn@gmail.com>
2021-04-24 14:35:21 +0700

    Timer

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { StyleSheet, Text, View } from "react-native";
 import Scanner from "./BarCodeScanner/Scanner";
 import Instagram from "./Instagram/index";
 import Game from "./KeoBuaBao/Index";
-
+import Timer from "./Timer/Timer";
 export default function App() {
   return (
     <>
       {/* <Instagram /> */}
       {/* <Game /> */}
-      <Scanner />
+      {/* <Scanner /> */}
+      <Timer />
     </>
   );
 }
